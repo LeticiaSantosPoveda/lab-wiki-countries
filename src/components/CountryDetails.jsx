7967b67d5e536000f586096304e459883af5be5d
Leticia Santos <leticia.s.poveda@gmail.com>
2022-10-27 20:14:40 +0200

    format good

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -7,43 +7,47 @@ function CountryDetails({countries}){
 
   const country = countries.find(country => country.alpha3Code === id)
 
-  return(
-        <div className="col-7">
-            <h1>{country.name.official}</h1>
-            <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt="country flag"/>
-            <table className="table">
-              <thead></thead>
-              <tbody>
-                <tr>
-                  <td style={{width: "30%"}}>Capital</td>
-                  <td>{country.capital}</td>
-                </tr>
-                <tr>
-                  <td>Area</td>
-                  <td>
-                  {country.area} km
-                    <sup>2</sup>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Borders</td>
-                  <td>
+  return (
+    <div className="col-7">
+      <h1>{country.name.official}</h1>
+      <img
+        src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+        alt="country flag"
+      />
+      <table className="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td style={{ width: '30%' }}>Capital</td>
+            <td>{country.capital}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              {country.area} km
+              <sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul>
+                {country.borders.map((country) => {
+                  return (
                     <ul>
-                        {country.borders.map(country =>{
-                            return(
-                                <ul>
-                                <Link to={"/"+country}><li>{country}</li></Link> 
-                                </ul>
-                            )
-                        })}
-                        
+                      <Link to={'/' + country}>
+                        <li>{country}</li>
+                      </Link>
                     </ul>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-        </div>
-    )
+                  );
+                })}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default CountryDetails;
